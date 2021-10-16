ad1b8ce7bc1ab4075f5a4acab41001fcd96cdf32
namnguyendevfl <namnguyenfl990@gmail.com>
2021-10-16 15:17:33 -0400

    add validation for the sign up form

--- server/src/accounts/accounts.controller.js
+++ server/src/accounts/accounts.controller.js
@@ -1,5 +1,24 @@
 const service = require("./accounts.service");
+const validator = require('validator');
 
+function hasData(req, res, next) {
+    if (req.body.data) return next();
+    next({status:400, message: "body must have data property"})
+};
+
+function hasPassowrd (req, res, next){
+    const password = req.body.data.password;
+    if (password.length >= 6) return next();
+    next({status:400, message: "password is too short"})
+};
+
+function hasUserName (req, res, next){
+    const userName = req.body.data.userName;
+    if (!isNaN(userName)) return next();
+    if (validator.isEmail(userName)) return next();
+    next({status:400, message: "userName must be a phone number or an email address"})
+    
+};
 async function read(req,res, next) {
     const data = await service.read();
     res.json({
@@ -16,6 +35,6 @@ async function create(req, res, next) {
 
 
 module.exports = {
-    create,
+    create: [hasData, hasUserName, hasPassowrd, create],
     read,
   };
--- server/src/db/migrations/20211015222121_createAccountsTable.js
+++ server/src/db/migrations/20211015222121_createAccountsTable.js
@@ -2,7 +2,14 @@
 exports.up = function(knex) { 
     return knex.schema.createTable("accounts", (table) => {
         table.increments("user_id").primary();
-        table.text("username");
+        table.text("firstName");
+        table.text("surName");
+        table.text("ageDay");
+        table.text("ageMonth");
+        table.text("ageYear");
+        table.text("gender");
+        table.text("acceptTerm");
+        table.text("userName");
         table.text("password");
         table.timestamps(true, true);
     })
